0f28d37bee3e29e4f32e69504e2de269273afd32
DESKTOP-FCMOFM8\Dmitry <aurorabitbucket@gmail.com>
2017-08-04 01:05:38 +0300

    chathub upd

--- 16-signalr2/SignalRMvc/SignalRMvc/Hubs/ChatHub.cs
+++ 16-signalr2/SignalRMvc/SignalRMvc/Hubs/ChatHub.cs
@@ -11,15 +11,20 @@ namespace SignalRMvc.Hubs
     /*SignalR использует две модели взаимодействия сервера и клиента: Persistent Connection и хабы. В данном случае мы используем хабы. Для этого создаем свой хаб ChatHub, который наследуется от класса Hub.
       Во-первых, мы создаем список, который будет хранить подключенных к чату пользователей.
       Далее у нас определен ряд методов для отправки сообщений, подключения и отключения пользователей.
-Формат вызова методов клиента
-Вызов метода на всех клиентах: Clients.All.addMessage(name, message);
-Вызов метода только на текущем клиенте, который обратился к серверу: Clients.Caller.addMessage(name, message);
-Вызов метода на всех клиентах, кроме того, который обратился к серверу: Clients.Others.addMessage(name, message);
-Вызов метода только у клиента с определенным id: Clients.Client(Context.ConnectionId).addMessage(name, message);
-Вызов метода на всех клиентах, кроме клиента с определенным id: Clients.AllExcept(connectionId).addMessage(name, message);
-Вызов метода на всех клиентах указанной группы: Clients.Group(groupName).addMessage(name, message);
-Вызов метода на всех клиентах указанной группы, за исключением клиента, у которого id - connectionId: Clients.Group(groupName, connectionId).addMessage(name, message);
-Вызов метода на всех клиентах указанной группы, за исключением обратившегося к серверу клиента: Clients.OthersInGroup(groupName).addMessage(name, message);
+        Формат вызова методов клиента
+        Вызов метода на всех клиентах: Clients.All.addMessage(name, message);
+        Вызов метода только на текущем клиенте, который обратился к серверу: Clients.Caller.addMessage(name, message);
+        Вызов метода на всех клиентах, кроме того, который обратился к серверу: Clients.Others.addMessage(name, message);
+        Вызов метода только у клиента с определенным id: Clients.Client(Context.ConnectionId).addMessage(name, message);
+        Вызов метода на всех клиентах, кроме клиента с определенным id: Clients.AllExcept(connectionId).addMessage(name, message);
+        Вызов метода на всех клиентах указанной группы: Clients.Group(groupName).addMessage(name, message);
+        Вызов метода на всех клиентах указанной группы, за исключением клиента, у которого id - connectionId: Clients.Group(groupName, connectionId).addMessage(name, message);
+        Вызов метода на всех клиентах указанной группы, за исключением обратившегося к серверу клиента: Clients.OthersInGroup(groupName).addMessage(name, message);
+
+        //comment (Metanit) :hub - это просто логическая конструкция, своего рода точка, 
+        // через которую приложение получает данные от пользователей и может обращаться к подключенным клиентам
+        // как скрипт файл узнает к какому именно классу/методу на сервере обращаться и наоборот? - по названию хаба - $.connection.chatHub
+        // и по названию функций, которые аналогичны методам хаба
 
      */
     public class ChatHub : Hub
@@ -37,6 +42,10 @@ namespace SignalRMvc.Hubs
         {
             var id = Context.ConnectionId;
 
+            //if (Users.Count(x => x.ConnectionId == id) == 0)
+            //comment: лучше писать Any, т.к. Count будет бежать по всей коллекции в любом случае, вычисляя количество, 
+            // а Any остановится на первом же объекте, удовлетворяющем условии. 
+            // Когда мы работает с большими объемами данных, это дает серьезную разницу.
 
             if (!Users.Any(x => x.ConnectionId == id))
             {
