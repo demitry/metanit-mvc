7e42ce90aa591635f8e5dfdf0b91992dd22b3447
Unknown <dpoluektov@gmail.com>
2017-08-05 15:45:30 +0300

    SignalR persistent connection chat project

---
+++ 16-signalr2/PersistentConMvc/PersistentConMvc/Models/Data.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PersistentConMvc.Models
+{
+    public class Data
+    {
+        public string Name { get; set; } // Имя пользователя
+        public string Message { get; set; } // Сообщение пользователя
+    }
+}
---
+++ 16-signalr2/PersistentConMvc/PersistentConMvc/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using PersistentConMvc.Connection;
+
+[assembly: OwinStartup(typeof(PersistentConMvc.Startup))]
+
+namespace PersistentConMvc
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+            app.MapSignalR<ChatConnection>("/chat");
+            //В данном случае регистрируем маршрут, по которому затем будем обращаться из клиентской части.
+        }
+    }
+}
