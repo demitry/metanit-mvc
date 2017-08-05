aa468a389f731203764f396a5515cc69321d4475
Unknown <dpoluektov@gmail.com>
2017-08-05 16:13:00 +0300

    drawing app

---
+++ 16-signalr2/SignalRDraw/SignalRDraw/DrawHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using SignalRDraw.Models;
+
+namespace SignalRDraw
+{
+    /*С помощью метода Clients.AllExcept() мы можем исключить из рассылки сообщений того клиента, который собственно и прислал сообщений, и таким образом, избежать ситуации, когда у него будет дублироваться нарисованная линия. А метод addLine собственно и выполняет рыссылку всем подключенным клиентам и задействует на клиенте одноименный метод addLine, который выполняет рисование.*/
+    public class DrawHub : Hub
+    {
+        public void Send(Data data)
+        {
+            Clients.AllExcept(Context.ConnectionId).addLine(data);
+        }
+    }
+}
---
+++ 16-signalr2/SignalRDraw/SignalRDraw/Models/Data.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SignalRDraw.Models
+{
+    public class Data
+    {
+        // JsonProperty attribute is used for linking with the model data
+
+        [JsonProperty("startX")]
+        public float StartX { get; set; }
+
+        [JsonProperty("startY")]
+        public float StartY { get; set; }
+
+        [JsonProperty("endX")]
+        public float EndX { get; set; }
+
+        [JsonProperty("endY")]
+        public float EndY { get; set; }
+    }
+}
