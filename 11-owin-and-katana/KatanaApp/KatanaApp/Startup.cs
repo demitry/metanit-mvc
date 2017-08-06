03bfc01b5efc684af805b4520010be85ff272ced
Unknown <dpoluektov@gmail.com>
2017-08-06 19:46:58 +0300

    Katana and Owin Test test

---
+++ 11-owin-and-katana/KatanaApp/KatanaApp/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(KatanaApp.Startup))]
+
+namespace KatanaApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/html; charset=utf-8";
+                return context.Response.WriteAsync("<h2>Hello World!</h2>");
+            });
+        }
+    }
+}
---
+++ 11-owin-and-katana/KatanaConsoleApp/KatanaConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatanaConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Microsoft.Owin.Hosting.WebApp.Start<Startup>("http://localhost:8000"))
+            {
+                Console.WriteLine("Сервер запущен. Нажмите любую клавишу для завершения работы...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
