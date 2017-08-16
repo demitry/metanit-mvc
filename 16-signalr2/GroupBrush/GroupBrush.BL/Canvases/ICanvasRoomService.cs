81b823d27e74f5808e7a4bda21db6bcedcad6d45
Unknown <dpoluektov@gmail.com>
2017-08-16 19:36:15 +0300

    Start...

---
+++ 16-signalr2/GroupBrush/GroupBrush.BL/Canvases/ICanvasRoomService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupBrush.BL.Canvases
+{
+    interface ICanvasRoomService
+    {
+        //TODO: uncomment 
+        //CanvasBrushAction AddBrushAction(string canvasId, CanvasBrushAction brushData);
+        //CanvasSnapshot SyncToRoom(string canvasId, int currentPosition);
+        void AddUserToCanvas(string canvasId, string id);
+        void RemoveUserFromCanvas(string canvasId, string id);
+    }
+}
---
+++ 16-signalr2/GroupBrush/GroupBrush.BL/Users/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupBrush.BL.Users
+{
+    interface IUserService
+    {
+        int? CreateAccount(string userName, string password);
+        bool ValidateUserLogin(string userName, string password, out int? userId);
+        string GetUserNameFromId(int id);
+    }
+}
